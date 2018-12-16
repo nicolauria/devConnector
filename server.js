ad7cee871afbf8ab9338900f1612e473024bb9e2
nico <nicolauria@outlook.com>
2018-12-15 17:40:10 -0800

    add profile model

--- server.js
+++ server.js
@@ -1,21 +1,32 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('hello world'));
-
-const users = require('./routes/api/users');
-const profile = require('./routes/api/profile');
-const posts = require('./routes/api/posts');
-
-app.use('/api/users', users);
-app.use('/api/profile', profile);
-app.use('/api/posts', posts);
-
+// connect express server
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`server running on port ${port}`));
 
+// connect to mongoDB
 const mongoose = require('mongoose');
 const db = require('./config/keys.js').mongoURI;
 mongoose.connect(db)
   .then(() => console.log('mongoDB connected'))
   .catch(err => console.log(err));
+
+// parse body of post requests
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
+// use routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
+// passport middleware
+const passport = require('passport');
+require('./config/passport')(passport);
+app.use(passport.initialize());
